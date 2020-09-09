4b09fe623ed3772947ac164675b2d9a6025de3bf
ruberchiles <36668479+ruberchiles@users.noreply.github.com>
2020-09-09 14:14:43 -0500

    Add files via upload

---
+++ proyecto/static/js/index.js
@@ -0,0 +1,75 @@
+//https://www.eclipse.org/paho/clients/js/
+
+function LED1_On() {
+	
+	console.log("led on");
+        message = new Paho.MQTT.Message("LED ON");
+        message.destinationName = "ruberchiles@hotmail.es/test1";
+        client.send(message);
+        
+  
+}
+function LED1_Off(){	
+	
+	console.log("led off");
+        message = new Paho.MQTT.Message("LED OFF")
+        message.destinationName = "ruberchiles@hotmail.es/test1";
+        client.send(message);
+}
+
+
+
+
+
+
+// Create a client instance
+  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
+  
+  client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  var options = {
+   useSSL: false,
+    userName: "ruberchiles@hotmail.es",
+    password: "Campeones1",
+    onSuccess:onConnect,
+    onFailure:doFail
+  }
+
+  // connect the client
+  client.connect(options);
+   
+  // called when the client connects
+  function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("Conectado...");
+	
+    client.subscribe("ruberchiles@hotmail.es/test");
+    message = new Paho.MQTT.Message("hola desde la web");
+    message.destinationName = "ruberchiles@hotmail.es/test1";
+    client.send(message);
+	
+  }
+
+  function doFail(e){
+    console.log(e);
+	
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+    console.log("Nuevo mensaje:"+message.payloadString);
+    document.getElementById("sensor").innerHTML=message.payloadString.split("=")[1];
+    document.getElementById("sensor1").innerHTML=message.payloadString.split("=")[1];
+
+  }
+  
